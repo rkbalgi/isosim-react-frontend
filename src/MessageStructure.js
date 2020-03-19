a9c248633cb088db574848f4f02717d4b6392f52
Raghavendra Balgi <rkbalgi@gmail.com>
2020-03-19 23:59:09 +0530

    Changes for loading messages from data store

--- src/MessageStructure.js
+++ src/MessageStructure.js
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from "axios";
 //import axios from 'axios'
 import IsoField from './IsoField.js'
-import Button from 'react-bootstrap/Button'
+import SelectMessageDialog from './SelectMessageDialog.js'
+import {Button, Modal} from 'react-bootstrap';
 
 export default class MessageStructure extends React.Component {
 
@@ -17,12 +18,60 @@ export default class MessageStructure extends React.Component {
       loaded: false,
       spec: props.spec,
       msg: props.msg,
-      shouldShow: props.showMsgTemplate
+      shouldShow: props.showMsgTemplate,
+      targetServerIp: '127.0.0.1',
+      targetServerPort: '6666',
+      mliType: "2I",
+      errDialogVisible: false,
+      errorMessage: '',
+      showLoadMessagesDialog: false
     };
+
     this.onFieldUpdate = this.onFieldUpdate.bind(this);
     this.appendFieldContent = this.appendFieldContent.bind(this);
     this.sendToHost = this.sendToHost.bind(this);
     this.addFieldContent = this.addFieldContent.bind(this);
+
+    this.serverIpChanged = this.serverIpChanged.bind(this);
+    this.serverPortChanged = this.serverPortChanged.bind(this);
+    this.mliTypeChanged = this.mliTypeChanged.bind(this);
+    this.showErrorDialog = this.showErrorDialog.bind(this);
+    this.closeErrorDialog = this.closeErrorDialog.bind(this);
+    this.processError = this.processError.bind(this);
+    this.showLoadMessagesDialog = this.showLoadMessagesDialog.bind(this);
+    this.closeLoadMsgDialog = this.closeLoadMsgDialog.bind(this);
+
+  }
+
+  closeLoadMsgDialog(selectedMsg) {
+    this.setState({showLoadMessagesDialog: false});
+    console.log("selected msg = ", selectedMsg);
+    //TODO:: now make another ajax call, let the msg and display it
+
+  }
+
+  showLoadMessagesDialog() {
+    this.setState({showLoadMessagesDialog: true})
+  }
+
+  closeErrorDialog() {
+    this.setState({errDialogVisible: false})
+  }
+
+  showErrorDialog() {
+    this.setState({errDialogVisible: true});
+  }
+
+  mliTypeChanged(e) {
+    this.setState({mliType: e.target.value});
+  }
+
+  serverIpChanged(e) {
+    this.setState({targetServerIp: e.target.value});
+  }
+
+  serverPortChanged(e) {
+    this.setState({targetServerPort: e.target.value});
   }
 
   addFieldContent(field, content) {
@@ -45,8 +94,36 @@ export default class MessageStructure extends React.Component {
     this.state.msgTemplate.Fields.forEach(f => {
       this.addFieldContent(f, content);
     });
-    console.log(content)
+    console.log(content);
 
+    let postData = 'host=' +
+        this.state.targetServerIp + '&' + 'port=' + this.state.targetServerPort
+        + '&' + 'mli=' + this.state.mliType
+        + '&' + 'specId=' + this.state.spec.Id + "&" + 'msgId='
+        + this.state.msg.Id + "&"
+        + "msg="
+        + JSON.stringify(content);
+    axios.post('http://localhost:8080/iso/v0/send', postData).then(res => {
+      console.log("Response from server", res);
+    }).catch(
+        e => {
+          console.log("error = ", e);
+          this.processError(e)
+        })
+
+  }
+
+  processError(e) {
+    if (e.response.status === 400) {
+      this.setState(
+          {errorMessage: e.response.data, errDialogVisible: true});
+    } else {
+      this.setState(
+          {
+            errorMessage: 'Unexpected error from server - '
+                + e.response.status, errDialogVisible: true
+          });
+    }
   }
 
   onFieldUpdate(e) {
@@ -91,7 +168,8 @@ export default class MessageStructure extends React.Component {
           console.log("MsgTemplate = ", this.state.msgTemplate);
 
           //this.forceUpdate()
-        }).catch(err => console.log(err))
+        }).catch(
+        err => this.setState({errorMessage: err, errDialogVisible: true}))
   }
 
   appendFieldContent(content, field, isoMsg, level) {
@@ -118,18 +196,78 @@ export default class MessageStructure extends React.Component {
           alignSelf: 'center',
           fill: 'aqua'
         }}>
+
+          <Modal show={this.state.errDialogVisible}
+                 onHide={this.closeErrorDialog}>
+            <Modal.Header closeButton>
+              <Modal.Title>Error</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{this.state.errorMessage}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.closeErrorDialog}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          <SelectMessageDialog show={this.state.showLoadMessagesDialog}
+                               specId={this.state.spec.Id}
+                               msgId={this.state.msg.Id}
+                               closeLoadMsgDialog={this.closeLoadMsgDialog}
+          />
+
           <div align="center"
                style={{
-                 height: "50px",
+                 height: "100px",
                  verticalAlign: "baseline",
                  alignItems: "center",
                  margin: "10px"
                }}>
 
-            <Button size={"sm"}>Parse Raw</Button>{' '}
-            <Button size={"sm"}>Load Message</Button>{' '}
-            <Button size={"sm"}>Save Message</Button>{' '}
-            <Button size={"sm"} onClick={this.sendToHost}>Send</Button>
+
+            <table
+                style={{fontFamily: 'ptserif-regular', fontSize: '14px'}}>
+              <tr>
+                <td><label style={{width: '60px'}}>Server Ip </label>{'   '}
+                  <input type="text" value={this.state.targetServerIp}
+                         onChange={this.serverIpChanged}/></td>
+                <td><label style={{width: '80px'}}>Server Port </label>
+                  <input type="text" value={this.state.targetServerPort}
+                         onChange={this.serverPortChanged}/></td>
+                <td><label style={{width: '70px'}}>MLI Type </label>
+
+                  <select value={this.state.mliType}
+                          onChange={this.mliTypeChanged}>
+                    <option key={"2i"} value={"2I"}>2I</option>
+                    <option key={"2e"} value={"2E"}>2E</option>
+                  </select></td>
+              </tr>
+            </table>
+
+            <table>
+
+              <tr>
+                <td>
+
+                </td>
+              </tr>
+
+
+              <tr>
+                <td>
+                  <Button size={"sm"}>Parse Raw</Button>{' '}
+                  <Button size={"sm"} onClick={this.showLoadMessagesDialog}>Load
+                    Message</Button>{' '}
+                  <Button size={"sm"}>Save Message</Button>{' '}
+                  <Button size={"sm"} onClick={this.sendToHost}>Send</Button>
+
+
+                </td>
+              </tr>
+
+
+            </table>
+
 
           </div>
           <table border="0">
---
+++ src/SelectMessageDialog.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import axios from 'axios'
+import {Button, Modal} from "react-bootstrap";
+
+export default class SelectMessageDialog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {show: props.show, selectedMsg: ''};
+    this.closeDialogSuccess = this.closeDialogSuccess.bind(this);
+    this.closeDialogFail = this.closeDialogFail.bind(this);
+    this.selectedMsgChanged = this.selectedMsgChanged.bind(this);
+  }
+
+  selectedMsgChanged(event) {
+    this.setState({selectedMsg: event.target.value});
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    //console.log("smd: componentDidUpdate", this.state);
+    if (this.props.show === true && prevState.show === false) {
+
+      axios.get('http://localhost:8080/iso/v0/loadmsg', {
+        params: {
+          specId: this.props.specId,
+          msgId: this.props.msgId,
+        }
+      }).then(res => {
+        console.log(res);
+        this.setState(
+            {savedMsgs: res.data, selectedMsg: res.data[0], show: true});
+
+      }).catch(e => {
+            //FIXME
+            console.log(e);
+            this.setState({show: true, errorMessage: e.response.data});
+          }
+      )
+    }
+  }
+
+  closeDialogSuccess() {
+    this.setState({show: false});
+    this.props.closeLoadMsgDialog(this.state.selectedMsg);
+  }
+
+  closeDialogFail() {
+    this.setState({show: false});
+    //TODO:: also tell the parent that we're done
+    //and return the value of the selected saved msg
+    this.props.closeLoadMsgDialog(null);
+  }
+
+  render() {
+
+    let content;
+
+    if (this.state.show) {
+      if (this.state.errorMessage) {
+        content = <div>{this.state.errorMessage}</div>
+      } else {
+        content =
+            <React.Fragment>
+              <label style={{fontFamily: "ptserif-regular"}}> Saved
+                Message </label>{'  '}
+              <select style={{fontFamily: "ptserif-regular", width: "200px"}}
+                      value={this.state.selectedMsg}
+                      onChange={this.selectedMsgChanged}>
+                {this.state.savedMsgs.map((sm) => {
+                  return <option key={sm} value={sm}>{sm}</option>
+                })
+                }
+              </select>
+            </React.Fragment>
+      }
+    }
+
+    return (
+        <Modal show={this.state.show}
+               onHide={this.closeDialogFail}>
+          <Modal.Header closeButton>
+            <Modal.Title>Load Saved Message</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{content}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.closeDialogSuccess}>
+              OK
+            </Button>
+            <Button variant="secondary" onClick={this.closeDialogFail}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+    );
+  }
+
+}
